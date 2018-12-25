43b845370f5d0300569f5c21133fdf71d8254c22
bou <khalil_bou33@hotmail.fr>
2018-12-25 16:22:42 +0100

    version1
    Création des couches DAL(EntityFramework), BOL et DTO.

--- BoVoyage_DAL/Data.cs
+++ BoVoyage_DAL/Data.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,6 @@ namespace BoVoyage_DAL
 {
     public class DataPool
     {
+        public static BVContext BoVContext = new BVContext();  
     }
 }
