92f6cb5fc20b4a5a1d7ff8876f3b01b969a6a64b
bou <khalil_bou33@hotmail.fr>
2018-12-25 13:06:38 +0100

    TestBV
    Test

---
+++ BoVoyage_DTO/DtoBoVoyage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyage_DTO
+{
+    public class DtoBoVoyage
+    {
+    }
+}
